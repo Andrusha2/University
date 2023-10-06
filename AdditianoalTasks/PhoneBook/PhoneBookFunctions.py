e27b616cca8a2e355fd4205cf7cc81ef4513e1bf
andrusha2 <herr.dron@mail.ru>
2023-10-06 16:15:52 +0300

    phonebook

--- AdditianoalTasks/PhoneBook/PhoneBookFunctions.py
+++ AdditianoalTasks/PhoneBook/PhoneBookFunctions.py
@@ -1,9 +1,28 @@
 phoneBook = {}
+
+
+def namestd(name):
+    result = ''
+    for i in range(len(name.split())):
+        result += name[i][0].upper() + name[i][1:].lower() + ' '
+    return result
+
+
+def numstd(number):
+    return "+7" + str(number)[-10:]
+
+
 def add(name, number):
-    phoneBook[name] = number
+    phoneBook[namestd(name)] = numstd(number)
+
+
 def delete(name):
-    del phoneBook[name]
+    del phoneBook[namestd(name)]
+
+
 def check():
     tuple(map(lambda x: print(*x), phoneBook.items()))
+
+
 def change(name):
-    phoneBook[name] = input("Type new number")
+    phoneBook[namestd(name)] = numstd(input("Type new number here: "))
--- AdditianoalTasks/PhoneBook/viewer.py
+++
@@ -1,3 +0,0 @@
-from PhoneBookFunctions import *
-while True:
-    pass
