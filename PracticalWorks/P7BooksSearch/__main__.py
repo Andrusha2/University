285606f0fc73448d81f7b739219e82d59fac7e79
DESKTOP-29N874D\herrd <herr.dron@mail.ru>
2023-11-24 09:52:20 +0300

    del

--- PracticalWorks/P7BooksSearch/__main__.py
+++ PracticalWorks/P7BooksSearch/__main__.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     min_sum = int(input('Enter the minimum amount: '))
     add = int(input('Enter how many should be added: '))
     result = get_totals(lst, min_sum, add)
-    print(result)
     with open(input('Enter the name of the new file: '), 'w') as f:
         writer = csv.writer(f, delimiter=',', lineterminator='\r')
         for i in result:
